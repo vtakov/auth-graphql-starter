ea16a43bfe21c7837d78059fab5b1a37f51559a4
vladimir Takov <vladimir.takov@gmail.com>
2019-09-23 14:09:20 +0300

    Chapter 97

---
+++ client/components/AuthForm.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class AuthForm extends React.Component {
+    constructor (props) {
+        super(props);
+
+        this.state = { email: '', password: '' };
+    }
+
+    render() {
+        return (
+            <div className="row">
+                <form className="col s4">
+                    <div className="input-field">
+                        <label>Email</label>
+                        <input
+                            value={this.state.email}
+                            onChange={e => this.setState({ email: e.target.value })}
+                        />
+                    </div>
+                    <div className="input-field">
+                        <label>Password</label>
+                        <input
+                            value={this.state.password}
+                            onChange={e => this.setState({ password: e.target.value })}
+                            type="password"
+                        />
+                    </div>
+                    <button className="btn">Submit</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default AuthForm;
--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import AuthForm from './AuthForm.js';
 
 class LoginForm extends React.Component {
     render() {
         return (
             <div>
-                LoginForm
+                <h3>Simply Login</h3>
+                <AuthForm/>
             </div>
         );
     }
