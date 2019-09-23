a66b56c0409bef410dbbe1087bcd9cad72f318d3
vladimir Takov <vladimir.takov@gmail.com>
2019-09-23 13:59:32 +0300

    Chapter 96

---
+++ client/components/LoginForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class LoginForm extends React.Component {
+    render() {
+        return (
+            <div>
+                LoginForm
+            </div>
+        );
+    }
+}
+
+export default LoginForm;
--- client/index.js
+++ client/index.js
@@ -4,6 +4,7 @@ import ApolloClient, { createNetworkInterface } from 'apollo-client'
 import { ApolloProvider } from 'react-apollo';
 import { Router, hashHistory, Route, IndexRoute } from 'react-router';
 import App from './components/App';
+import LoginForm from './components/LoginForm.js';
 
 const networkInterface = createNetworkInterface({
     uri: '/graphql',
@@ -21,7 +22,9 @@ const Root = () => {
     return (
         <ApolloProvider client={client}>
             <Router history={hashHistory}>
-                <Route path="/" component={App}></Route>
+                <Route path="/" component={App}>
+                    <Route path="login" component={LoginForm}></Route>
+                </Route>
             </Router>
         </ApolloProvider>
     );
